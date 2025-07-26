7dc18348e359971293a8b144a44c9cb4b9c55ad1
JuanRibeiro <Juan.1997d@hotmail.com>
2025-07-26 14:58:00 -0300

    feat adicao de esqueleto e estrutura docker

---
+++ api-reembolsos/src/main/java/com/estudo/aws/api_reembolsos/ApiReembolsosApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.aws.api_reembolsos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReembolsosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReembolsosApplication.class, args);
+	}
+
+}
---
+++ api-reembolsos/src/test/java/com/estudo/aws/api_reembolsos/ApiReembolsosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.aws.api_reembolsos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReembolsosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
